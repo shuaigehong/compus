f51c037bfb9eea1bf5e9ecaae0e4b167ef2b11ae
shuaigehong <2608415174@qq.com>
2015-11-25 14:14:31 +0800

    第一次提交用于测试

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by shuaigehong on 2015/11/25.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("你好啊");
+        System.out.println("你真的好帅");
+    }
+}
