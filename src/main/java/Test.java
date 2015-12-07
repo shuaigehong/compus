1b160fb0cca101cd9d48ad3f31265bc254748053
shuaigehong <2608415174@qq.com>
2015-12-07 16:39:12 +0800

    删除测试版本

--- src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by shuaigehong on 2015/11/25.
- */
-public class Test {
-    public static void main(String[] args){
-        System.out.println("你好啊");
-        System.out.println("你真的好帅");
-    }
-}
